c154f5eb652f560d1a959e8ebd7da203ca7822db
fredericomenezes.mail@gmail.com <fredericomenezes.mail@gmail.com>
2014-01-31 19:50:45 -0200

    textos com acentuacao

--- server/comunideia/app/controllers/users_controller.rb
+++ server/comunideia/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @feed_items = @user.feed.paginate(page: params[:page])
+    #@feed_items = @user.feed.paginate(page: params[:page])
     @thumbnails = @user.feed
   end
 
