025327e38c60822f1b15db77e25e3c7a828ba42e
fredericomenezesmail <fredericomenezes.mail@gmail.com>
2014-03-17 12:44:33 -0300

    correcao de analise de cpf

--- server/comunideia/db/migrate/20140106230201_create_users.rb
+++ server/comunideia/db/migrate/20140106230201_create_users.rb
@@ -7,7 +7,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
       t.string :remember_token
 
-      t.string :cpf, :limit => 8
+      t.string :cpf
       t.date :birth_date
       t.string :address
       t.integer :address_num
